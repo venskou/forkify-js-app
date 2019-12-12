fc320f3317cc0f5666db70f40cd4735aef486224
Uladzislau Vensko <ulad.vensko@gmail.com>
2019-12-12 19:28:00 +0300

    Display search results

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,24 @@
-// Global app controller
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements, renderLoader, clearLoader } from './views/base';
+
+const state = {};
+
+const controlSearch = async () => {
+  const query = searchView.getInput();
+
+  if (query) {
+    state.search = new Search(query);
+    searchView.clearInput();
+    searchView.clearResults();
+    renderLoader(elements.searchRes);
+    await state.search.getResults();
+    clearLoader();
+    searchView.renderResults(state.search.result);
+  }
+};
+
+elements.searchForm.addEventListener('submit', e => {
+  e.preventDefault();
+  controlSearch();
+});
