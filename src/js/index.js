b0e9500e0c86b0fb8919c87a853dec5a2870b5eb
Uladzislau Vensko <ulad.vensko@gmail.com>
2019-12-13 17:15:51 +0300

    Implement Recipe controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,14 @@
 import Search from './models/Search';
+import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 const state = {};
 
+/*
+ * SEARCH CONTROLLER
+ * */
 const controlSearch = async () => {
   const query = searchView.getInput();
 
@@ -12,9 +17,14 @@ const controlSearch = async () => {
     searchView.clearInput();
     searchView.clearResults();
     renderLoader(elements.searchRes);
-    await state.search.getResults();
-    clearLoader();
-    searchView.renderResults(state.search.result);
+
+    try {
+      await state.search.getResults();
+      clearLoader();
+      searchView.renderResults(state.search.result);
+    } catch (error) {
+      console.log('error', error);
+    }
   }
 };
 
@@ -29,5 +39,46 @@ elements.searchResPages.addEventListener('click', e => {
     const goToPage = parseInt(btn.dataset.goto, 10);
     searchView.clearResults();
     searchView.renderResults(state.search.result, goToPage);
+  }
+});
+
+/*
+ * RECIPE CONTROLLER
+ * */
+const controlRecipe = async () => {
+  const id = window.location.hash.replace('#', '');
+
+  if (id) {
+    recipeView.clearRecipe();
+    renderLoader(elements.recipe);
+    if (state.search) {
+      searchView.highlightSelected(id);
+    }
+    state.recipe = new Recipe(id);
+
+    try {
+      await state.recipe.getRecipe();
+      state.recipe.parseIngredients();
+      state.recipe.calcTime();
+      state.recipe.calcServings();
+      clearLoader();
+      recipeView.renderRecipe(state.recipe);
+    } catch (error) {
+      console.log('error', error);
+    }
+  }
+};
+
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+
+elements.recipe.addEventListener('click', e => {
+  if (e.target.matches('.btn-decrease, .btn-decrease *')) {
+    if (state.recipe.servings > 1) {
+      state.recipe.updateServings('dec');
+      recipeView.updateServingsIngredients(state.recipe);
+    }
+  } else if (e.target.matches('.btn-increase, .btn-increase *')) {
+    state.recipe.updateServings('inc');
+    recipeView.updateServingsIngredients(state.recipe);
   }
 });
